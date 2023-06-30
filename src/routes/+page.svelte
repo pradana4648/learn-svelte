88bf58d92eaad73acf941b72a73d7a1ea0316646
pradana4648 <wakwaujaim@gmail.com>
2023-06-30 21:18:20 +0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import "../app.css";
+  type Notebook = {
+    readonly id: number;
+    readonly name: string;
+  };
+
+  let notebooks: Notebook[] = [];
+
+  function generateRandom() {
+    const randomId = Math.floor(Math.random() * 100);
+    const data: Notebook = {
+      id: randomId,
+      name: "Test " + randomId,
+    };
+    notebooks = [...notebooks, data];
+  }
+</script>
+
+<div class="container mx-auto p-5">
+  <div class="text-center">
+    <h1 class="font-bold text-xl">Welcome to Notebook</h1>
+    <blockquote class="mx-auto max-w-xl italic m-2">
+      "In here you can create your dream for your brain desire, even though it
+      is not possible by You, but God can make it happen to you if you try hard
+      as possible""
+    </blockquote>
+    <!-- <div>Here are list you can build</div> -->
+    <button
+      on:click={generateRandom}
+      class="p-2 border border-black rounded active:bg-gray-200"
+      >Generate Random</button
+    >
+    <div>
+      {#each notebooks as nb}
+        <p class="m-2">{nb.name}</p>
+      {/each}
+    </div>
+  </div>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<p>You clicked this {count}</p>
+<button on:click={handleClick}>Click Me</button>
